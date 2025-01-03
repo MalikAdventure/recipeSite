563d916ed5b90b82bcfc8d581b805241c82442ab
Malik Anton <malikantonit@gmail.com>
2025-01-03 15:40:34 +0700

    Add location and ip determination

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -8,6 +8,12 @@ import Link from 'next/link'
 
 import { useAppDispatch, useAppSelector } from '@/hooks/redux'
 import { setShowModal } from '@/store/reducers/modalSlice'
+import {
+	setPosition,
+	setCountry,
+	setRegion,
+	setCity,
+} from '@/store/reducers/contextSlice'
 
 import Image from 'next/image'
 
@@ -60,6 +66,63 @@ const Profile: FC = () => {
 
 	const auth = sessionStorage.getItem('profile')
 
+	const { position } = useAppSelector((state) => state.contextReducer)
+
+	useEffect(() => {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(
+				(position) => {
+					dispatch(
+						setPosition({
+							latitude: position.coords.latitude,
+							longitude: position.coords.longitude,
+						})
+					)
+				},
+				() => {
+					console.error('Не удалось получить координаты.')
+				}
+			)
+		} else {
+			alert('Ваш браузер не поддерживает Geolocation API.')
+		}
+	}, [])
+
+	const country = sessionStorage.getItem('country')
+	const region = sessionStorage.getItem('region')
+	const city = sessionStorage.getItem('city')
+
+	useEffect(() => {
+		if (country === null && region === null && city === null) {
+			// Получаем IP-адрес через api.ipify.org
+			fetch('https://api.ipify.org?format=json')
+				.then((response) => response.json())
+				.then((data) => {
+					const userIp = data.ip
+					// Используем ipinfo.io для получения данных о регионе
+					fetch(`https://ipinfo.io/${userIp}/json`)
+						.then((response) => response.json())
+						.then((geoData) => {
+							if (geoData.country) {
+								dispatch(setCountry(geoData.country))
+							}
+							if (geoData.region) {
+								dispatch(setRegion(geoData.region))
+							}
+							if (geoData.city) {
+								dispatch(setCity(geoData.city))
+							}
+						})
+						.catch((error) => {
+							console.error('Ошибка при получении данных о геолокации:', error)
+						})
+				})
+				.catch((error) => {
+					console.error('Ошибка при получении IP-адреса:', error)
+				})
+		}
+	}, [])
+
 	return (
 		<>
 			<div className='profile'>
@@ -76,6 +139,20 @@ const Profile: FC = () => {
 								? 'Пользователь'
 								: 'Гость'}
 						</span>
+					</p>
+					<p className='profile__item description-text'>
+						{position.latitude !== 0 && position.longitude !== 0
+							? `Местоположение: ${position.latitude} ${position.longitude}`
+							: 'Местоположение не определено'}
+					</p>
+					<p className='profile__item description-text'>
+						{country !== null ? `Страна: ${country}` : 'Страна не определена'}
+					</p>
+					<p className='profile__item description-text'>
+						{region !== null ? `Регион: ${region}` : 'Регион не определен'}
+					</p>
+					<p className='profile__item description-text'>
+						{city !== null ? `Город: ${city}` : 'Город не определен'}
 					</p>
 					{!auth ? (
 						<Link href='/authorization' className='profile__item'>
--- src/store/reducers/contextSlice.ts
+++ src/store/reducers/contextSlice.ts
@@ -3,11 +3,19 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 export interface ContextState {
 	contextPage: string
 	profile: { name: string; email: string; status: string }
+	position: { latitude: number; longitude: number }
+	country: string | null
+	region: string | null
+	city: string | null
 }
 
 const initialState: ContextState = {
 	contextPage: '',
 	profile: JSON.parse(sessionStorage.getItem('profile') || '{}'),
+	position: { latitude: 0, longitude: 0 },
+	country: sessionStorage.getItem('country') || null,
+	region: sessionStorage.getItem('region') || null,
+	city: sessionStorage.getItem('city') || null,
 }
 
 export const contextSlice = createSlice({
@@ -28,8 +36,33 @@ export const contextSlice = createSlice({
 			state.profile = action.payload
 			sessionStorage.setItem('profile', JSON.stringify(state.profile))
 		},
+		setPosition: (
+			state,
+			action: PayloadAction<{ latitude: number; longitude: number }>
+		) => {
+			state.position = action.payload
+		},
+		setCountry: (state, action: PayloadAction<string>) => {
+			state.country = action.payload
+			sessionStorage.setItem('country', state.country)
+		},
+		setRegion: (state, action: PayloadAction<string>) => {
+			state.region = action.payload
+			sessionStorage.setItem('region', state.region)
+		},
+		setCity: (state, action: PayloadAction<string>) => {
+			state.city = action.payload
+			sessionStorage.setItem('city', state.city)
+		},
 	},
 })
 
-export const { setContextPage, setProfile } = contextSlice.actions
+export const {
+	setContextPage,
+	setProfile,
+	setPosition,
+	setCountry,
+	setRegion,
+	setCity,
+} = contextSlice.actions
 export default contextSlice.reducer
